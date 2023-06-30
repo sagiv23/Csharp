0fd41604c007373e4f8d2a761a6116a7ac316fca
Sagiv23 <104361570+sagiv23@users.noreply.github.com>
2023-06-30 10:39:26 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,37 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && window.matchMedia("(orientation: portrait)").matches) {
+  window.addEventListener("load",function(){
+    swal("!חכו", "!אנא סובב את המסך למצב רוחבי עבור חווית הצפייה הטובה ביותר", "error");
+  }); 
+}
+
+var Title = new Typed(".headline", {
+  strings:["Sagiv23"],
+  typeSpeed : 100 , 
+  backSpeed : 100 , 
+  loop:true
+})
+
+function Title_qu() {
+  document.getElementById("_qu").style.display = 'block';
+  document.getElementById("_qu").scrollIntoView({ behavior: 'smooth'});
+}
+
+function qu_urls() {
+  document.getElementById("_urls").style.display = 'block';
+  document.getElementById("_urls").scrollIntoView({ behavior: 'smooth'});
+}
+
+function urls_qu() {
+  document.getElementById("_qu").style.display = 'block';
+  document.getElementById("_qu").scrollIntoView({ behavior: 'smooth'});
+}
+
+function T() {
+  document.getElementById("T").style.display = 'block';
+  document.getElementById("T").scrollIntoView({ behavior: 'smooth'});
+}
+
+function T_In_The_Page() {
+  document.getElementById("_In_The_Page").style.display = 'block';
+  document.getElementById("_In_The_Page").scrollIntoView({ behavior: 'smooth'});
+}
